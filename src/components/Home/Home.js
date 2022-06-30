ab846a3c06b143edbb0634409944c6627feea57d
Torikul Islam <toriqulislam142@gmail.com>
2022-06-30 09:25:25 -0500

    implemented to submit form on press Enter key

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 
+
 const Home = () => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
 
-    const handleTaskSubmit = (event)=>{
-        event.preventDefault()
-        const title = event.target.title.value
-        const description = event.target.description.value
-        const todo = {title,description}
-       fetch('http://localhost:5000/addTodos',{
-        method:'POST',
-        headers:{
-            'content-type':'application/json'
-        },
-        body:JSON.stringify(todo)
-       })
-       .then(res => res.json())
-       .then(data => {
-        if(data.acknowledged){
-            event.target.reset()
-            alert('Todo task added')
-        }
-        else{
-            alert('Something went wrong')
-        }
-       })
+    useEffect(() => {
+        const listener = event => {
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
 
+                event.preventDefault();
+
+                handleTaskSubmit(event)
+            }
+        };
+        document.addEventListener("keydown", listener);
+        return () => {
+            document.removeEventListener("keydown", listener);
+        };
+    });
+
+    const handleTaskSubmit = (e) => {
+        e.preventDefault()
+        const todo = { title, description }
+        fetch('http://localhost:5000/addTodos', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    document.getElementById('title').value = ''
+                    document.getElementById('description').value = ''
+                    alert('Todo task added')
+
+                }
+                else {
+                    alert('Something went wrong')
+                }
+            })
     }
     return (
-        <div>
-            <form onSubmit={handleTaskSubmit}>
-                <input name='title' type="text" placeholder='Title' className='d-block' required />
-                <textarea  className='d-block' name="description" placeholder='Your task description heres' cols="30" rows="10" required></textarea>
-                <input type="submit" value="Add Task" />
-            </form>
-        </div>
+        <form onSubmit={handleTaskSubmit} >
+            <input onChange={(e) => setTitle(e.target.value)} id='title' type="text" placeholder='Title' className='d-block' required />
+            <textarea onChange={(e) => setDescription(e.target.value)} className='d-block' id='description' placeholder='Your task description heres' cols="30" rows="10" required></textarea>
+            <button type='submit'>Add Todo</button>
+        </form>
     );
 };
 
