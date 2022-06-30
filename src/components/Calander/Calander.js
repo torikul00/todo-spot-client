0ca4f04167c41994b8fba86f837cecf05668988c
Torikul Islam <toriqulislam142@gmail.com>
2022-06-30 05:31:05 -0500

    install day-picher and show calander in calander route

--- src/components/Calander/Calander.js
+++ src/components/Calander/Calander.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './Calander.css'
-const Calander = () => {
-    return (
-        <div>
-            Calander
-        </div>
-    );
-};
+import React, { useState } from 'react';
 
-export default Calander;
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css'
+
+export default function Example() {
+  const [selected, setSelected] = useState('')
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={footer}
+    />
+  );
+}
--- src/components/MyTodo/MyTodo.js
+++ src/components/MyTodo/MyTodo.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MyTodo.css'
 const MyTodo = () => {
+    const [todos,setTodos] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/todos')
+        .then(res => res.json())
+        .then(data =>setTodos(data))
+    },[])
+
+
     return (
         <div>
-            my todo
+        <h1></h1>
         </div>
     );
 };
